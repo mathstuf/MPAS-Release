57b11810768c7911564f8f00ea1d028e81842186
Ben Boeckel <ben.boeckel@kitware.com>
2014-01-31 18:19:00 -0500

    Add an mpas_common and mpas-example script
    These scripts are the initial attempt at making the Python script more
    declarative than normal scripts. Completely untested.

---
+++ src/paraview_catalyst/Examples/mpas-example.py
@@ -0,0 +1,174 @@
+try: paraview.simple
+except: from paraview.simple import *
+
+from mpas_common import *
+
+# A dictionary of datasets to export is used to determine what is wanted when
+# running a simulation. Each entry in the dictionary describes a grid to export
+# along with information describing how it should be exported.
+datasets = {
+    # This is the name associated with the output. It is used in the default
+    # filename patterns if another name is not given.
+    'example': {
+        ########################################################################
+        # Core information
+        #-----------------------------------------------------------------------
+        # REQUIRED: This is required and is the name of the grid to export from
+        # the simulation. Each entry may only export a single grid, but the
+        # same grid may be used multiple times in different outputs.
+        'layer': 'LON_LAT_1LAYER-primal',
+
+        ########################################################################
+        # Image exporting
+        #-----------------------------------------------------------------------
+        # REQUIRED: How often an image should be written. If not provided, no
+        # images will be created.
+        'image_frequency': 5,
+        # OPTIONAL: The filename pattern to use when writing images. Use '%t'
+        # to use the timestep of the image. The file format is automatically
+        # determined from the file extension. The default is 'NAME_%t.png'
+        # where NAME is the key of the output (the key in 'datasets').
+        'image_pattern': 'example_%t.jpg',
+
+        ########################################################################
+        # Filters
+        #-----------------------------------------------------------------------
+        # Filters can be used to modify the incoming data before writing it
+        # out. Filters are applied before *any* writers, so if the data type is
+        # modified, remember to change the writer used.
+        #-----------------------------------------------------------------------
+        # OPTIONAL: The list of filters to apply, in order.
+        'filters': [
+            {
+                ################################################################
+                # Filters may be used to modify the simulation data before
+                # writing it out. In this example, a filter to do the following:
+                #     filter = PassArrays(guiName='PassArrays1',
+                #                         CellDataArrays=['salinity'])
+                #     filter.property = 4
+                #---------------------------------------------------------------
+                # REQUIRED: A callable value which constructs a filter
+                'function': PassArrays,
+                # OPTIONAL: Positional arguments to be passed to the function.
+                'args': [],
+                # OPTIONAL: Keyword arguments to be passed to the function.
+                'kwargs': {
+                    'guiName': 'PassArrays1',
+                    'CellDataArrays': ['salinity']
+                },
+                # OPTIONAL: Properties to set on the filter.
+                'properties': {
+                    'property': 4
+                }
+            }
+        ],
+
+        ########################################################################
+        # Grid exporting
+        #-----------------------------------------------------------------------
+        # REQUIRED: How often a grid should be written. If not provided, no
+        # grids will be created.
+        'grid_frequency': 5,
+        # OPTIONAL: The writer class to use when writing out the grid. The
+        # default is to use an XMLPUnstructuredGridWriter since all MPAS
+        # outputs are unstructured grids.
+        'grid_class': XMLPUnstructuredGridWriter,
+        # OPTIONAL: The filename pattern to use when writing grid files. Use
+        # '%t' to use the timestep of the grid. The default is 'NAME_%t.pvtu'
+        # where NAME is the key of the output (the key in 'datasets').
+        'grid_pattern': 'example_lonlat1_%t.pvtu',
+
+        ########################################################################
+        # In-situ exporting
+        #-----------------------------------------------------------------------
+        # These options will configure how images for use in the in-situ web
+        # viewer will be exported.
+        #-----------------------------------------------------------------------
+        # REQUIRED: How often in-situ data should be written using the slice
+        # exporter. If not provided, no slice data will be exported.
+        'in_situ_slice_frequency': 5,
+        # REQUIRED: The directory to export images into. It must exist prior to
+        # execution. Relative paths are interpreted from the run directory of
+        # the simulation.
+        'in_situ_slice_dir': 'in_situ-slice',
+        # REQUIRED: The file pattern to use when writing images. The string is
+        # formatted using Python's str.format() method. The 'sliceColor' and
+        # 'slicePosition' keys are required.
+        'in_situ_slice_pattern': '{time}_{sliceColor}_{slicePosition}.png',
+        # REQUIRED: The colors to use when rendering images.
+        # TODO: Have Sebastion document these fields.
+        'in_situ_slice_colors': {
+            # The name of the lookup table entry.
+            'temperature': {
+                'lut': GetLookupTableForArray(
+                        # The name of the array.
+                        'temperature',
+                        # The number of components.
+                        1,
+                        # TODO: Have Sebastion document these fields.
+                        RGBPoints=[0, 0.0, 0.0, 1.0, 28.658824920654297, 1.0, 0.0, 0.0],
+                        VectorMode='Magnitude',
+                        NanColor=[0.498039, 0.498039, 0.498039],
+                        ColorSpace='HSV',
+                        ScalarRangeInitialized=1.0),
+                "type": 'CELL_DATA'
+            }
+        },
+        # OPTIONAL: The number of slices to export (default 10).
+        'in_situ_slice_slices': 30,
+        # OPTIONAL: The normal vector for the slice plane (default [0, 0, 1]).
+        # TODO: Sebastian?
+        'in_situ_slice_normal': [0, 0, 1],
+        # OPTIONAL: The up vector for the camera (default [0, 1, 0]).
+        # TODO: Sebastian?
+        'in_situ_slice_viewup': [0, 1, 0],
+        # OPTIONAL: The bounds range for the data (default [0, 1]).
+        # TODO: Sebastian?
+        'in_situ_slice_bound_range': [0, 1],
+        # OPTIONAL: Undocumented (default 2).
+        # TODO: Sebastian?
+        'in_situ_slice_scale_ratio': 2,
+
+        #-----------------------------------------------------------------------
+        # XXX: Needs testing.
+        # ThreeSixtyImageExporter.
+        #-----------------------------------------------------------------------
+        # REQUIRED: How often in-situ data should be written using the rotation
+        # exporter. If not provided, no rotation data will be exported.
+        'in_situ_rotate_frequency': 5,
+        # REQUIRED: The directory to export images into. It must exist prior to
+        # execution. Relative paths are interpreted from the run directory of
+        # the simulation.
+        'in_situ_rotate_dir': 'in_situ-rotate',
+        # REQUIRED: The file pattern to use when writing images. The string is
+        # formatted using Python's str.format() method. The 'phi' and 'theta'
+        # keys are required.
+        'in_situ_rotate_pattern': '{time}_{phi}_{theta}.png',
+        # OPTIONAL: Where the camera should stare (default [0, 0, 0]).
+        'in_situ_rotate_focal_point': [0, 0, 0],
+        # OPTIONAL: The distance the camera orbits around the focal point
+        # (default 100).
+        'in_situ_rotate_distance': 100,
+        # OPTIONAL: The axis around which the camera rotates.
+        'in_situ_rotate_axis': [0, 0, 1],
+        # OPTIONAL: The phi and theta steps (in degrees) the camera rotates
+        # each step (phi is the angle around the main axis and theta is the
+        # angle around the axis between the camera and the focal point)
+        # (default [10, 15]).
+        'in_situ_rotate_step': [10, 15]
+    }
+}
+
+# Remove the example from the datasets in case it is wanted for documentation.
+if 'example' in datasets:
+    del datasets['example']
+
+coprocessor = MPASCreateCoProcessor(datasets)
+
+# These functions is required and is called from Catalyst without arguments.
+# Instead, pass the datasets we want to export to MPASCreateCoProcessor.
+def RequestDataDescription(datadescription):
+    return MPASRequestDataDescription(coprocessor, datadescription)
+
+def DoCoProcessing(datadescription):
+    return MPASDoCoProcessing(coprocessor, datadescription)
---
+++ src/paraview_catalyst/Examples/mpas_common.py
@@ -0,0 +1,175 @@
+try: paraview.simple
+except: from paraview.simple import *
+
+from paraview import coprocessing
+from paraview import data_exploration as wx
+
+import math
+
+DEFAULT_VIEW_OPTIONS = {
+    'fit_to_screen': 1,
+    'magnification': 1.0,
+    'width': 960,
+    'height': 540,
+}
+DEFAULT_VIEW_PROPERTIES = {
+    'CacheKey': 0.0,
+    'StereoType': 0,
+    'UseLight': 1,
+    'StereoRender': 0,
+    'CameraPosition': [15000.0, 99592.9248046875, 647834.944767225],
+    'StereoCapableWindow': 0,
+    'CameraClippingRange': [441356.5953195527, 1361552.4689387335],
+    'LightSwitch': 0,
+    'ViewTime': 0.0,
+    'Background': [0, 0, 0],
+    'CameraFocalPoint': [15000.0, 99592.9248046875, -200000.0],
+    'CameraParallelScale': 219435.83080920158,
+    'CenterOfRotation': [15000.0, 99592.9248046875, -200000.0],
+    'ViewSize': [1000, 500],
+    'OrientationAxesVisibility': 0,
+    'CenterAxesVisibility': 0
+}
+
+def MPASCreateCoProcessor(datasets, options={}):
+    freqs = {}
+    for (name, dataset) in datasets.items():
+        layer = dataset['layer']
+
+        # Prepare the frequency map.
+        if layer not in freqs:
+            freqs[layer] = []
+
+        # Any frequencies must be known here.
+        if 'image_frequency' in dataset:
+            freqs[layer].append(dataset['image_frequency'])
+        if 'grid_frequency' in dataset:
+            freqs[layer].append(dataset['grid_frequency'])
+        if 'in_situ_frequency' in dataset:
+            freqs[layer].append(dataset['in_situ_frequency'])
+
+    def mpas_create_pipeline(coprocessor, datadescription):
+        class MPASPipeline(object):
+            for (name, dataset) in datasets.items():
+                layer = dataset['layer']
+
+                image_pattern = dataset.get('image_pattern', '%s_%%t.png' % name)
+                # Use pi if no images are wanted since it will never have a
+                # zero modulo with an integer. Using zero causes
+                # ZeroDivisionError exceptions in coprocessing.
+                image_frequency = dataset.get('image_frequency', math.pi)
+
+                view_options = DEFAULT_VIEW_OPTIONS.copy()
+                if 'view' in options:
+                    view_options.update(options['view'])
+
+                view_props = DEFAULT_VIEW_PROPERTIES.copy()
+                if 'view_properties' in options:
+                    view_props.update(options['view_properties'])
+
+                view = coprocessor.CreateView(CreateRenderView,
+                        image_pattern,
+                        image_frequency,
+                        view_options['fit_to_screen'],
+                        view_options['magnification'],
+                        view_options['width'],
+                        view_options['height'])
+                for (k, v) in view_props.items():
+                    setattr(view, k, v)
+
+                producer = coprocessor.CreateProducer(datadescription, layer)
+                SetActiveSource(producer)
+
+                if 'filters' in dataset:
+                    for filter_desc in dataset['filters']:
+                        args = filter_desc.get('args', [])
+                        kwargs = filter_desc.get('kwargs', {})
+                        filt = filter_desc['function'](*args, **kwargs)
+                        if 'properties' in filter_desc:
+                            for (k, v) in filter_desc['properties'].items():
+                                setattr(filt, k, v)
+
+                # Support for in-situ image exporting
+                if 'in_situ_slice_frequency' in dataset:
+                    def slice_wrapper(view, producer, dataset):
+                        def create_slice_explorer():
+                            file_generator = wx.FileNameGenerator(dataset['in_situ_slice_dir'], dataset['in_situ_slice_pattern'])
+                            return wx.SliceExplorer(file_generator, view, producer,
+                                    dataset['in_situ_slice_colors'],
+                                    dataset.get('in_situ_slice_slices', 10),
+                                    dataset.get('in_situ_slice_normal', [0, 0, 1]),
+                                    dataset.get('in_situ_slice_viewup', [0, 1, 0]),
+                                    dataset.get('in_situ_slice_bound_range', [0, 1]),
+                                    dataset.get('in_situ_slice_scale_ratio', 2))
+                        return create_slice_explorer
+                    in_situ_exporter = coprocessor.CreateWriter(slice_wrapper(view, producer, dataset),
+                            '', dataset['in_situ_slice_frequency'])
+
+                # Support for in-situ image exporting
+                # XXX: Disabled for now. Will this work?
+                if False and 'in_situ_rotate_frequency' in dataset:
+                    def rotate_wrapper(view, producer, dataset):
+                        def create_rotate_explorer():
+                            file_generator = wx.FileNameGenerator(dataset['in_situ_rotate_dir'], dataset['in_situ_rotate_pattern'])
+                            return wx.ThreeSixtyImageExporter(file_generator, view,
+                                    dataset.get('in_situ_rotate_focal_point', [0, 0, 0]),
+                                    dataset.get('in_situ_rotate_distance', 100),
+                                    dataset.get('in_situ_rotate_axis', [0, 0, 1]),
+                                    dataset.get('in_situ_rotate_step', [10, 15]))
+                        return create_rotate_explorer
+                    in_situ_exporter = coprocessor.CreateWriter(rotate_wrapper(view, producer, dataset),
+                            '', dataset['in_situ_rotate_frequency'])
+
+                if 'grid_frequency' in dataset:
+                    grid_class = dataset.get('grid_class', XMLPUnstructuredGridWriter)
+                    grid_pattern = dataset.get('grid_pattern', '%s_%%t.pvtu' % name)
+                    grid_frequency = dataset['grid_frequency']
+
+                    coprocessor.CreateWriter(grid_class, grid_pattern, grid_frequency)
+
+        return MPASPipeline()
+
+    class MPASCoProcessor(coprocessing.CoProcessor):
+        def CreatePipeline(self, datadescription):
+            self.Pipeline = mpas_create_pipeline(self, datadescription)
+
+    coprocessor = MPASCoProcessor()
+    coprocessor.SetUpdateFrequencies(freqs)
+
+    # Enable Live-Visualizaton with ParaView
+    coprocessor.EnableLiveVisualization(False)
+
+    return coprocessor
+
+# ---------------------- Data Selection method ----------------------
+
+def MPASRequestDataDescription(coprocessor, datadescription):
+    "Callback to populate the request for current timestep"
+    if datadescription.GetForceOutput() == True:
+        # We are just going to request all fields and meshes from the simulation
+        # code/adaptor.
+        for i in range(datadescription.GetNumberOfInputDescriptions()):
+            datadescription.GetInputDescription(i).AllFieldsOn()
+            datadescription.GetInputDescription(i).GenerateMeshOn()
+        return
+
+    # setup requests for all inputs based on the requirements of the
+    # pipeline.
+    coprocessor.LoadRequestedData(datadescription)
+
+# ------------------------ Processing method ------------------------
+
+def MPASDoCoProcessing(coprocessor, datadescription):
+    "Callback to do co-processing for current timestep"
+    # Update the coprocessor by providing it the newly generated simulation data.
+    # If the pipeline hasn't been setup yet, this will setup the pipeline.
+    coprocessor.UpdateProducers(datadescription)
+
+    # Write output data, if appropriate.
+    coprocessor.WriteData(datadescription);
+
+    # Write image capture (Last arg: rescale lookup table), if appropriate.
+    coprocessor.WriteImages(datadescription, rescale_lookuptable=False)
+
+    # Live Visualization, if enabled.
+    coprocessor.DoLiveVisualization(datadescription, "localhost", 22222)
